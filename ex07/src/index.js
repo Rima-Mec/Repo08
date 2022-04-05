c560880678a98702c868674668380bfa57f044e0
Rima-Mec <dmerima08@gmail.com>
2022-04-05 10:18:33 +0200

    Adding index.js file

---
+++ ex07/src/index.js
@@ -0,0 +1,15 @@
+// Only change code belowe this line
+
+var myVariable = dress;
+var dress ="little dress";
+
+function myDress() {
+    var myVariable = dress;
+    var dress = "ball gown";
+    return myVariable;
+}
+// Only change this code above this line
+
+console.log(myDress());
+
+module.exports = myDress;
